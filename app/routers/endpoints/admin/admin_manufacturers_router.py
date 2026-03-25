c48ea986d9246736cc74db080f655cec8df74e81
dbernacki <daw.bernacki@gmail.com>
2026-03-25 23:43:56 +0100

    Refactor admin manufacturers router: use `Annotated` for dependency injection and simplify `Depends` usage.

--- app/routers/endpoints/admin/admin_manufacturers_router.py
+++ app/routers/endpoints/admin/admin_manufacturers_router.py
@@ -1,7 +1,6 @@
-from typing import List
+from typing import List, Annotated
 
-from fastapi import APIRouter
-from fastapi.params import Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -16,40 +15,42 @@ router = APIRouter(
     tags=["Admin - manufacturer"]
 )
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[ManufacturerReadDto])
-async def find_all_manufacturers(db: Session = Depends(get_db)):
+async def find_all_manufacturers(db: db_dependency):
     service = AdminManufacturerService(db)
     return service.get_all_manufacturers()
 
 
 @router.get("/{manufacturer_id}", status_code=status.HTTP_200_OK, response_model=ManufacturerReadDto)
-async def find_manufacturer_by_id(manufacturer_id: int, db: Session = Depends(get_db)):
+async def find_manufacturer_by_id(manufacturer_id: int, db: db_dependency):
     service = AdminManufacturerService(db)
     return service.get_manufacturer_by_id(manufacturer_id)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_manufacturer(manufacturer_create_dto: ManufacturerCreateDto, db: Session = Depends(get_db)):
+async def create_manufacturer(manufacturer_create_dto: ManufacturerCreateDto, db: db_dependency):
     service = AdminManufacturerService(db)
     service.create_manufacturer(manufacturer_create_dto)
 
 
 @router.put("/{manufacturer_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def create_manufacturer(manufacturer_id: int, manufacturer_update_dto: ManufacturerUpdateDto,
-                              db: Session = Depends(get_db)):
+                              db: db_dependency):
     service = AdminManufacturerService(db)
     service.update_manufacturer_all_fields(manufacturer_id, manufacturer_update_dto)
 
 
 @router.patch("/{manufacturer_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def create_manufacturer(manufacturer_id: int, manufacturer_update_dto: ManufacturerUpdateDto,
-                              db: Session = Depends(get_db)):
+                              db: db_dependency):
     service = AdminManufacturerService(db)
     service.update_manufacturer_separate_fields(manufacturer_id, manufacturer_update_dto)
 
 
 @router.delete("/{manufacturer_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_manufacturer_by_id(manufacturer_id: int, db: Session = Depends(get_db)):
+async def delete_manufacturer_by_id(manufacturer_id: int, db: db_dependency):
     service = AdminManufacturerService(db)
     service.delete_manufacturer_by_id(manufacturer_id)
