e3127408142d96fc1270aa7e7ce8a2a825b1bd86
dbernacki <daw.bernacki@gmail.com>
2026-03-26 22:16:06 +0100

    Add bike details page: implement `render_bike_details` route, integrate `ManufacturerService`, update template and styles with new bike details layout.

--- app/routers/render_pages/front/homepage_bike_router.py
+++ app/routers/render_pages/front/homepage_bike_router.py
@@ -8,6 +8,7 @@ from starlette.templating import Jinja2Templates
 from app.database.database import get_db
 from app.schemas.admin.bike.admin_bike_list_request_dto import BikeListRequestDto
 from app.services.front.bike_service import BikeService
+from app.services.front.manufacturer_service import ManufacturerService
 
 router = APIRouter(
     prefix="/bikes",
@@ -34,5 +35,19 @@ async def render_bikes(request: Request, db: db_dependency):
             "size": pagination.size,
             "total": pagination.total,
             "pages": pagination.pages,
+        },
+    )
+
+@router.get("/{bike_id}", status_code=status.HTTP_200_OK)
+async def render_bike_details(request: Request, bike_id: int, db: db_dependency):
+    bike = BikeService(db).get_bike_by_id(bike_id)
+    manufacturers = ManufacturerService(db).get_all_manufacturers()
+
+    return templates.TemplateResponse(
+        "front/bikes/bike_details.html",
+        {
+            "request": request,
+            "bike": bike,
+            "manufacturers": manufacturers,
         },
     )
