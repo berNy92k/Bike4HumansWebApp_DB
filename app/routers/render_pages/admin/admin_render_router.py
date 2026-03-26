7976d6eca43d158e2a3ece8ff5c56c051f2e3f61
dbernacki <daw.bernacki@gmail.com>
2026-03-26 01:48:08 +0100

    Update admin homepage: replace static counts with dynamic values for bikes, manufacturers, users, and roles; update icons and notes in the template; enhance page rendering with additional service integrations.

--- app/routers/render_pages/admin/admin_render_router.py
+++ app/routers/render_pages/admin/admin_render_router.py
@@ -7,6 +7,8 @@ from starlette.templating import Jinja2Templates
 from app.database.database import get_db
 from app.routers.utils.admin_utils_router import redirect_to_login
 from app.services.admin.admin_bike_service import AdminBikeService
+from app.services.admin.admin_manufacturer_service import AdminManufacturerService
+from app.services.admin.admin_user_service import AdminUserService
 from app.services.auth.auth_service import AuthService
 
 router = APIRouter(
@@ -25,9 +27,14 @@ async def render_admin_homepage(request: Request, db: db_dependency):
     try:
         await AuthService(db).validate_access(request)
 
-        bikes_short_list = AdminBikeService(db).get_last_x_bikes(10)
-
         return templates.TemplateResponse("admin/homepage/index.html",
-                                          {"request": request, "bikesShortList": bikes_short_list})
+                                          {
+                                              "request": request,
+                                              "bikesShortList": AdminBikeService(db).get_last_x_bikes(10),
+                                              "bikeSize": len(AdminBikeService(db).get_all_bikes()),
+                                              "manufacturerSize": len(AdminManufacturerService(db).get_all_manufacturers()),
+                                              "userSize": len(AdminUserService(db).get_all_users()),
+                                              "roleSize": len(AdminUserService(db).get_all_roles())
+                                          })
     except HTTPException:
         return redirect_to_login()
