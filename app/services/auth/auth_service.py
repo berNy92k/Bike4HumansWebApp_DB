c011660508c113872ee64501404204fc2458b805
dbernacki <daw.bernacki@gmail.com>
2026-03-29 21:01:56 +0200

    Load environment variables using `python-dotenv`, replace hardcoded secrets and database URL with `.env` config for improved security and configurability.

--- app/services/auth/auth_service.py
+++ app/services/auth/auth_service.py
@@ -1,6 +1,8 @@
+import os
 from datetime import datetime, timezone, timedelta
 from typing import Annotated
 
+from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
@@ -11,8 +13,7 @@ from starlette import status
 from app.models import User
 from app.services.auth.user_service import UserService
 
-SECRET_KEY = "aADASDsadasd213123123"
-ALGORITHM = "HS256"
+load_dotenv()
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -32,7 +33,7 @@ async def get_current_user_from_cookie(request: Request):
 
 def _decode_jwt_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")])
         username: str = payload.get("sub")
         user_id: str = payload.get("id")
         role_id: str = payload.get("role_id")
@@ -51,7 +52,7 @@ def generate_jwt_token(username: str, user_id: int, role_id: int, expire_delta: 
     expire = datetime.now(timezone.utc) + expire_delta
     token.update({"exp": expire})
 
-    return jwt.encode(token, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(token, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
 
 
 class AuthService:
@@ -61,7 +62,6 @@ class AuthService:
 
     def authenticate_user(self, username: str, password: str) -> User:
         user: User = self.user_service.find_user_by_username(username)
-        print(user)
         if not bcrypt_context.verify(password, str(user.hashed_password)):
             raise HTTPException(status_code=401, detail="User is not authorized")
 
